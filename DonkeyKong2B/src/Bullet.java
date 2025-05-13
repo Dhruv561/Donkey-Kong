70e45b4b2c63ebabdcd70105fc8ed88ffad76113
Dhruv Verma <104663445+Dhruv561@users.noreply.github.com>
2025-05-13 14:26:35 +1000

    fixed bug with bullets travelling forever

--- DonkeyKong2B/src/Bullet.java
+++ DonkeyKong2B/src/Bullet.java
@@ -3,16 +3,19 @@ import bagel.*;
 public class Bullet extends GameObject {
     private boolean isDestroyed = false;
     private boolean isRight = false;
+    private double startingX;
     private final static Image LEFT_SPRITE = new Image("res/bullet_left.png");
     private final static Image RIGHT_SPRITE = new Image("res/bullet_right.png");
     private final static int BULLET_GRAVITY = 0;
     private final static int BULLET_TERMINAL_VELOCITY = 0;
     private final static double MOVEMENT_VELOCITY = 3.8;
+    private final static double MAXIMUM_PIXELS = 300;
     private final static int OFFSCREEN = -100;
 
     public Bullet(double centreX, double centreY, boolean isRight) {
         super(centreX, centreY, isRight ? RIGHT_SPRITE : LEFT_SPRITE, BULLET_GRAVITY, BULLET_TERMINAL_VELOCITY);
         this.isRight = isRight;
+        this.startingX = getCentreX();
     }
 
     public boolean isDestroyed() {
@@ -56,10 +59,14 @@ public class Bullet extends GameObject {
     }
 
     public void update(NormalMonkey[] normalMonkeys, IntelligentMonkey[] intelligentMonkeys, DonkeyKong donkeyKong, GameStats stats) {
-        if (isRight) {
-            setCentreX(getCentreX() + MOVEMENT_VELOCITY);
+        if (Math.abs(startingX - getCentreX()) >= MAXIMUM_PIXELS) {
+            destroy();
         } else {
-            setCentreX(getCentreX() - MOVEMENT_VELOCITY);
+            if (isRight) {
+                setCentreX(getCentreX() + MOVEMENT_VELOCITY);
+            } else {
+                setCentreX(getCentreX() - MOVEMENT_VELOCITY);
+            }
         }
         if (getCentreX() >= ShadowDonkeyKong.getScreenWidth() || getCentreX() <= 0) {
             isDestroyed = true;
